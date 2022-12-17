8d19756c11aeac02400cfdc91e9d869b9a81da1b
krrr017 <xfxrx.531@gmail.com>
2022-12-17 15:17:28 +0900

    somechnages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
+import { addPost, deletePost } from "./features/Posts";
+import { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+  const [content, setContent] = useState("");
+
+
+  const postList = useSelector((state) => state.posts.value);
+  const dispatch = useDispatch();
+
+  const handleClick = () => {
+    dispatch(
+      addPost({
+        id: postList.length + 1,
+        name: name,
+        content: content,
+      })
+    );
+
+    setName("");
+    setContent("");
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1 className="title">KEIJIBAN BORAD</h1>
+      </div>
+      <div className="addPost">
+        <input
+          type="text"
+          placeholder="Name"
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+        />
+        <input
+          type="text"
+          placeholder="Post"
+          onChange={(e) => setContent(e.target.value)}
+          value={content}
+        />
+        <button onClick={() => handleClick()}>POST</button>
+        <hr />
+      </div>
+      <div className="displayPosts">
+        {postList.map((post) => (
+          <div key={post.id} className="post">
+            <h1 className="postName">{post.name}</h1>
+            <h1 className="postContent">{post.content}</h1>
+            <button onClick={() => dispatch(deletePost({ id: post.id }))}>
+              DELETE
+            </button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/DummyData.js
@@ -0,0 +1,22 @@
+export const PostsData = [
+    {
+    id: 1,
+    name: "Reina",
+    content: "It's snowing!",
+    },
+    {
+    id: 2,
+    name: "Aki",
+    content: "Too cold.....",
+    },
+    {
+    id: 3,
+    name: "unknown",
+    content: "Any plans for winter?",
+    },
+    {
+    id: 4,
+    name: "unknown",
+    content: "Gotta go snowboarding!",
+    },
+];
